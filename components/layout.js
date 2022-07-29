35faec4adef1a42fc5c5490ff174579e7aec52d8
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-07-29 20:12:22 +0600

    🏷️added header

--- components/layout.js
+++ components/layout.js
@@ -1,33 +1,10 @@
-import { Container } from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Image from "next/image";
-import Logo from "../public/static/Logo.svg";
-import AppFooter from "../views/AppFooter";
+import Header from "../views/Header";
 
 export default function Layout({ children }) {
-  const containerStyles = {
-    // mx: 0,
-    px: 0,
-    // mb: 10,
-    // ml: 4,
-    // mr: 4,
-    // display: "flex",
-    // position: "relative",
-    backgroundColor: "red",
-  };
-
   return (
     <>
-      <AppBar position="static" color="secondary">
-        <Container sx={containerStyles} disableGutters maxWidth="lg">
-          <Toolbar disableGutters>
-            <Image src={Logo} layout="intrinsic" alt="logo" />
-          </Toolbar>
-        </Container>
-      </AppBar>
+      <Header />
       <main>{children}</main>
-      <AppFooter />
     </>
   );
 }
